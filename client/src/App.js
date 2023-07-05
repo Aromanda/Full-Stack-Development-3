f1cd7074d7a8bf16d4fdf3711f3645f81bfb6d2e
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-04 23:42:49 -0400

    create transaction page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Edit from "./components/edit";
 import Create from "./components/create";
 import Login from "./components/login";
 import Error from "./components/error";
+import BodyOnlyExample from "./components/transaction";
 import { AgentsListCard, TransactionsCard } from "./components/admin";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -38,6 +39,7 @@ const App = () => {
        <Route path="/create" element={<Create />} />
        <Route path="/error" element={<Error />} />
        <Route path="/admin" element={<AdminPage />} />
+       <Route path="/transaction" element={<BodyOnlyExample />} />
      </Routes>
    </div>
  );
