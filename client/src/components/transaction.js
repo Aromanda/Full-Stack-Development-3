168be686448024d3d9fd0255cc7b2dca43642a10
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-04 23:44:39 -0400

    create transaction page

---
+++ client/src/components/transaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BodyOnlyExample() {
+    return (
+      <Card>
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+    );
+  }
+  
+  export default BodyOnlyExample;
