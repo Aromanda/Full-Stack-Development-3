8791fd3e5170f33f72298f0f058ecaacafc0e360
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-06 13:04:03 -0400

    create session schema

--- server/db/session.schema.mjs
+++ server/db/session.schema.mjs
@@ -1,1 +1,23 @@
+import mongoose from 'mongoose';
+
+// Définition du schéma
+const sessionSchema = new mongoose.Schema({
+  session_token: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  }
+});
+
+// Création du modèle à partir du schéma
+const Session = mongoose.model('Session', sessionSchema);
+
+// Export du modèle
+export default Session;
+
 
