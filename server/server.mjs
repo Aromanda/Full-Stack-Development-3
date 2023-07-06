ead073b0ab10d3a10fe148aa9e83e92e5e726389
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-06 13:03:49 -0400

    install session express

--- server/server.mjs
+++ server/server.mjs
@@ -1,8 +1,10 @@
 import express from "express";
+import session from 'express-session';
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import Record from "./routes/record.routes.mjs";
 import Users from "./routes/users.routes.mjs";
+import Session from "./routes/session.routes.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,8 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  session({
+    secret: 'rocket',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+    }
+  })
+);
+
 app.use("/record", Record);
 app.use("/users", Users);
+app.use("/session", Session);
+
 
 // start the Express server
 app.listen(PORT, () => {
