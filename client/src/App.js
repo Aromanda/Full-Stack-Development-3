d2ffe07bbd9e3a39568d765c4efe3d46eadb6cc5
Aromanda <aromandanyfitiaratoejanahary@yahoo.fr>
2023-07-07 16:48:29 -0400

    update get validatetoken

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-import React from "react";
- 
-// We use Route in order to define the different routes of our application
-import { Route, Routes } from "react-router-dom";
- 
-// We import all the components we need in our app
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
@@ -18,33 +14,64 @@ import { getCookie } from "react-use-cookie";
 
 function AdminPage() {
   return (
-      <Row>
-        <Col>
-          <AgentsListCard />
-        </Col>
-        <Col>
-          <TransactionsCard />
-        </Col>
-      </Row>
+    <Row>
+      <Col>
+        <AgentsListCard />
+      </Col>
+      <Col>
+        <TransactionsCard />
+      </Col>
+    </Row>
   );
 }
 
 const App = () => {
-  const cookie = getCookie("sessionToken");
- return (
-   <div>
-     <Navbar />
-     <Routes>
-       <Route exact path="/recordList" element={<RecordList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/" element={<Login />} />
-       <Route path="/create" element={<Create />} />
-       <Route path="/error" element={<Error />} />
-       <Route path="/admin" element={<AdminPage />} />
-       <Route path="/transaction" element={<BodyOnlyExample />} />
-     </Routes>
-   </div>
- );
+  const navigate = useNavigate();
+  const sessionToken = getCookie("sessionToken");
+
+  const [fullname, setFullname] = useState("");
+
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await fetch(`http://localhost:5050/session/validate_token?token=${sessionToken}`);
+        const data = await response.json();
+        console.log("ici");
+        console.log(data.data)
+        const { valid, user } = data.data;
+        const {first_name, last_name} = user;
+        setFullname(`${first_name} ${last_name}`);
+        console.log(fullname);
+        
+        if (!valid) {
+          navigate("/");
+        }
+        console.log(validateToken);
+      } catch (error) {
+        console.error("Une erreur s'est produite lors de la validation du token.");
+        navigate("/error");
+      }
+    };
+
+    validateToken();
+  }, [sessionToken, navigate]);
+
+  return (
+    <div>
+      <Navbar fullname={fullname} />
+      <Routes>
+        <Route exact path="/recordList" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/transaction" element={<BodyOnlyExample />} />
+      </Routes>
+    </div>
+  );
 };
- 
+
 export default App;
+
+
